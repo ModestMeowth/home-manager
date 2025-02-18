d2373344e3a217e9857039f9a6ce47a6bf3d97ef
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-02-18 11:34:59 -0600

    fix zellij autostart

--- modules/fish.nix
+++ modules/fish.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   programs.fish = {
     plugins = with pkgs.fishPlugins; [
       {
@@ -15,18 +15,21 @@
       }
     ];
 
-    interactiveShellInit = # fish
+    shellInit = # fish
+      ''
+        if not string match -qi "vscode" $TERM_PROGRAM
+          set ZELLIJ_AUTO_ATTACH true
+          set ZELLIJ_AUTO_EXIT true
+        end
+      '';
+
+    interactiveShellInit = lib.mkBefore # fish
       ''
         set -g fish_greeting
         fish_vi_key_bindings
 
         set fzf_preview_dir_cmd eza --always --color=always
         set -x FZF_DEFAULT_OPTS --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9,fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9,info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6,marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker=*
-
-        set ZELLIJ_AUTO_EXIT true
-        if not string match -qi "vscode" $TERM_PROGRAM
-          set ZELLIJ_AUTO_ATTACH true
-        end
       '';
 
     shellAbbrs = {
