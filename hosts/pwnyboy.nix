d80c03aeb9a63535d6c5aaf0259dbe4ce4c8543f
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-03-19 15:17:57 -0500

    fish abbrs for docker compose commands

--- hosts/pwnyboy.nix
+++ hosts/pwnyboy.nix
@@ -5,5 +5,15 @@
   programs = {
     k9s.enable = true;
     k9s.settings.k9s.skin = "dracula";
+
+    fish.shellAbbrs = {
+      dcd = "docker compose down";
+      dce = "docker compose exec";
+      dcl = "docker compose logs";
+      dcr = "docker compose restart";
+      dcu = "docker compose up -d";
+      dls = "docker compose ls";
+      dps = "docker compose ps";
+    };
   };
 }
