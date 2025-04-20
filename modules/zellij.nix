dfa8ced97429dc30637965597f25e2b9ae16c488
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-04-19 20:25:43 -0500

    zellij copy fix?

--- modules/zellij.nix
+++ modules/zellij.nix
@@ -4,31 +4,29 @@
   xdg.configFile = {
     "zellij/config.kdl".text = # kdl
       ''
-                support_kitty_keyboard_protocol false
-                default_mode "normal"
+        copy_command: "wl-copy"
+        default_mode "normal"
+        theme "dracula"
 
-                plugins {
-                  tab-bar {
-                    path "tab-bar"
-                  }
+        plugins {
+          tab-bar {
+            path "tab-bar"
+          }
 
-                  status-bar {
-                    path "status-bar'"
-        	        }
+          status-bar {
+            path "status-bar'"
+          }
 
-                  compact-bar {
-                    path "compact-bar"
-                  }
-                }
+          compact-bar {
+            path "compact-bar"
+          }
+        }
 
-                ui {
-                  pane_frames {
-                    rounded_corners true
-                  }
-                }
-
-
-                theme "dracula"
+        ui {
+          pane_frames {
+            rounded_corners true
+          }
+        }
       '';
 
     "zellij/layouts/default.kdl".text = # kdl
