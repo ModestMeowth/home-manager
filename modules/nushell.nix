5944b116d6415250d87ae6fe6a0af59217523f5f
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-05-11 01:16:18 -0500

    mosh fix?

--- modules/nushell.nix
+++ modules/nushell.nix
@@ -18,6 +18,9 @@
       cd = "__zoxide_z";
       ci = "__zoxide_zi";
       j = "just";
+      # Mosh doesn't play nice with nushell
+#      ssh = ''mosh --server="LANG=C.UTF-8 mosh-server"'';
+      ssh = "mosh";
     };
   };
 }
