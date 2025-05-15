0a1ac6ec73be022fac15679994f289723e85a9e3
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-05-15 18:09:29 -0500

    formatting

--- modules/neovim/keymaps.nix
+++ modules/neovim/keymaps.nix
@@ -44,58 +44,62 @@
       # Snacks
       {
         key = "<leader>ee";
-        action.__raw = "function() require\"snacks\".explorer() end";
+        action.__raw = ''function() require"snacks".explorer() end'';
         options.desc = "File Explorer";
       }
       {
         key = "<leader>pf";
-        action.__raw = "function() require\"snacks\".picker.files() end";
+        action.__raw = ''function() require"snacks".picker.files() end'';
         options.desc = "Find Files";
       }
       {
         key = "<leader>/";
-        action.__raw = "function() require\"snacks\".picker.grep() end";
+        action.__raw = ''function() require"snacks".picker.grep() end'';
         options.desc = "Grep";
       }
       {
         key = "<leader>pr";
-        action.__raw = "function() require\"snacks\".picker.registers() end";
+        action.__raw = ''function() require"snacks".picker.registers() end'';
         options.desc = "Registers";
       }
       {
         key = "gd";
-        action.__raw = "function() require\"snacks\".picker.lsp_definitions() end";
+        action.__raw = ''function() require"snacks".picker.lsp_definitions() end'';
         options.desc = "Goto Definition";
       }
       {
         key = "gD";
-        action.__raw = "function() require\"snacks\".picker.lsp_declarations() end";
+        action.__raw = ''function() require"snacks".picker.lsp_declarations() end'';
         options.desc = "Goto Declaration";
       }
       {
         key = "gr";
-        action.__raw = "function() require\"snacks\".picker.lsp_references() end";
+        action.__raw = ''function() require"snacks".picker.lsp_references() end'';
         options.nowait = true;
         options.desc = "References";
       }
       {
         key = "gI";
-        action.__raw = "function() require\"snacks\".picker.lsp_implementation() end";
+        action.__raw = # lua
+          ''function() require"snacks".picker.lsp_implementation() end'';
         options.desc = "Goto Implementation";
       }
       {
         key = "gy";
-        action.__raw = "function() require\"snacks\".picker.lsp_type_definitions() end";
+        action.__raw = # lua
+          ''function() require"snacks".picker.lsp_type_definitions() end'';
         options.desc = "Goto T[y]pe Definition";
       }
       {
         key = "<leader>ss";
-        action.__raw = "function() require\"snacks\".picker.lsp_symbols() end";
+        action.__raw = # lua
+          ''function() require"snacks".picker.lsp_symbols() end'';
         options.desc = "LSP Symbols";
       }
       {
         key = "<leader>sS";
-        action.__raw = "function() require\"snacks\".picker.lsp_workspace_symbols() end";
+        action.__raw = # lua
+        ''function() require"snacks".picker.lsp_workspace_symbols() end'';
         options.desc = "LSP Workspaces Symbols";
       }
    ];
