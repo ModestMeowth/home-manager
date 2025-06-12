7dacb1f59f0e651713e61e910229755b3d1e7456
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-06-12 17:33:34 -0500

    DAP UI

--- modules/neovim/debug.nix
+++ modules/neovim/debug.nix
@@ -1,5 +1,10 @@
-{
+{ pkgs, ...}: {
+  home.packages = with pkgs; [
+    delve
+  ];
+
   programs.nixvim.plugins = {
+    dap.enable = true;
     dap-ui.enable = true;
     dap-virtual-text.enable = true;
 
@@ -7,4 +12,22 @@
     dap-python.enable = true;
     dap-rr.enable = true;
   };
+
+  programs.nixvim.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>dt";
+      action.__raw = ''require"dap".toggle_breakpoint'';
+    }
+    {
+      mode = "n";
+      key = "<leader>dc";
+      action.__raw = ''require"dap".continue'';
+    }
+    {
+      mode = "n";
+      key = "<leader>ut";
+      action.__raw = ''function() require"dapui".toggle() end'';
+    }
+  ];
 }
